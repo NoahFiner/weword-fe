b8f96d50a39767fe2c254e0507e94c1aea1afeff
nfiner <nfiner@umich.edu>
2019-10-09 23:52:45 -0400

    add transitions, delays, error checking

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,6 +3,7 @@ import socketIOClient from "socket.io-client";
 import './App.scss';
 import Logo from '../Logo';
 import Footer from '../Footer';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 class App extends Component {
   constructor() {
@@ -10,8 +11,10 @@ class App extends Component {
     this.state = {
       words: [],
       // TODO THIS IS HELLA INSECURE
-      endpoint: process.env.NODE_ENV ? "https://weword-app.herokuapp.com/" : "http://127.0.0.1:4001",
+      // endpoint: process.env.NODE_ENV ? "https://weword-app.herokuapp.com/" : "http://127.0.0.1:4001",
+      endpoint: "http://127.0.0.1:4001",
       socket: null,
+      disabled: false,
     };
     console.log(process.env.NODE_ENV);
   }
@@ -24,6 +27,14 @@ class App extends Component {
     socket.on("sendWords", words => {
       this.setState({words});
     });
+
+    socket.on("disable", data => {
+      this.setState({disabled: true});
+    })
+
+    socket.on("enable", data => {
+      this.setState({disabled: false});
+    })
   }
 
   render() {
@@ -38,13 +49,17 @@ class App extends Component {
             <div className="divider"></div>
           </div>
           <div className="content">
-            {words.map((value, idx) => {
-              return <p key={idx} className="word">{value}</p>
-            })}
-            <div className="next-word"></div>
+            <TransitionGroup>
+              {words.map((value, idx) => {
+                return (<CSSTransition timeout={500} classNames="word">
+                  <p key={idx} className="word">{value}</p>
+                </CSSTransition>)
+              })}
+              <div className="next-word"></div>
+            </TransitionGroup>
           </div>
         </div>
-        <Footer socket={this.state.socket} />
+        <Footer inactive={this.state.disabled} socket={this.state.socket} />
       </div>
     );
   }
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,31 +4,39 @@ import './Footer.scss';
 class Footer extends Component {
   constructor(props) {
     super(props);
-    this.state = {word: ''}
+    this.state = {word: '', error: ''}
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({word: event.target.value});
+    this.setState({word: event.target.value, error: ''});
   }
 
   handleSubmit(event) {
     event.preventDefault();
     console.log('handling submit');
-    this.props.socket.emit("addWord", this.state.word);
-    this.setState({word: ''});
+    this.props.socket.emit("addWord", this.state.word, (error) => {
+      if(error) {
+        this.setState({error});
+      } else {
+        this.setState({word: ''});
+      }
+    });
   }
 
   render() {
     return (
-      <div className="footer">
+      <div className={"footer " + (this.props.inactive ? 'inactive':  '')}>
         <form onSubmit={this.handleSubmit}>
           <h1>Add a word:</h1>
-          <input type="text" onChange={this.handleChange} value={this.state.word} className="add-word-input"></input>
+          <input type="text" disabled={this.props.inactive} onChange={this.handleChange} value={this.state.word} className={"add-word-input " + (this.state.error ? 'error' : '')}></input>
           <input type="submit" value="submit" className="button"></input>
         </form>
+        <p id="error-msg">
+          {this.state.error}
+        </p>
       </div>
     );
   }
